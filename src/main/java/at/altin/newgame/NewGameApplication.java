3742c6112e61abad6d607cad55b5c7a5fd7e77b5
altin <kelmendialtin48@gmail.com>
2022-07-13 19:57:52 +0200

    Initial commit

---
+++ src/main/java/at/altin/newgame/NewGameApplication.java
@@ -0,0 +1,13 @@
+package at.altin.newgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/altin/newgame/NewGameApplicationTests.java
@@ -0,0 +1,13 @@
+package at.altin.newgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
