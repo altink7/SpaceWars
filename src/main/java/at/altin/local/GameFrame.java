336d7c2b19f752dd5f4a1b8886d96d9168a8a82c
altin <kelmendialtin48@gmail.com>
2022-07-13 20:07:23 +0200

    Base implemented

--- src/main/java/at/altin/local/GameFrame.java
+++ src/main/java/at/altin/local/GameFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class GameFrame extends JFrame {
     GameFrame(){
         this.add(new GamePanel());
-        this.setTitle("DrawPoint");
+        this.setTitle("Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
