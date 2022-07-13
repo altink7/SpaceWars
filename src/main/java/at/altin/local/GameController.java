63cedb0b980e7b19123ae1d45145c1166f0ebe90
altin <kelmendialtin48@gmail.com>
2022-07-13 20:06:58 +0200

    Base implemented

---
+++ src/main/java/at/altin/local/GameController.java
@@ -0,0 +1,7 @@
+package at.altin.local;
+
+public class GameController {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
--- src/main/java/at/altin/web/NewGameApplication.java
+++ src/main/java/at/altin/web/NewGameApplication.java
@@ -1,4 +1,4 @@
-package at.altin.newgame;
+package at.altin.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
