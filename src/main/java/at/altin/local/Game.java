60222813dabe2d92706ee913aaaf62c13d3ed4d3
altin <kelmendialtin48@gmail.com>
2022-07-22 21:12:01 +0200

    Basis Services implemented, Erweiterung um Animation und ClickArea, für verschiedene Applikationen einsetzbar.

--- src/main/java/at/altin/local/Game.java
+++ src/main/java/at/altin/local/Game.java
@@ -1,5 +1,8 @@
 package at.altin.local;
 import at.altin.local.display.Window;
+import at.altin.local.handlers.KeyHandler;
+import at.altin.local.handlers.MouseHandler;
+import at.altin.local.handlers.ObjectHandler;
 import at.altin.local.service.GraphicsLoader;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
@@ -36,6 +39,8 @@ public class Game extends Canvas implements Runnable{
     }
 
     public synchronized void start() {
+        this.addKeyListener(new KeyHandler());
+        this.addMouseListener(new MouseHandler());
         this.running = true;
         this.thread = new Thread();
         this.thread.start();
@@ -46,6 +51,32 @@ public class Game extends Canvas implements Runnable{
         score=0;
         gameover=false;
         running=true;
+        img_welcome = GraphicsLoader.readGraphics("C:\\Users\\User\\IdeaProjects\\NewGame\\welcome.png");
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+        } else {
+            Graphics g = bs.getDrawGraphics();
+            ObjectHandler.render(g);
+            g.fillRect(0,0,1200,750);
+            g.drawImage(img_welcome, 0, 0, null);
+            g.setFont(new Font("Arial", 1, 48));
+            g.setColor(Color.WHITE);
+            String s = "Press Space";
+            int textWidth = g.getFontMetrics().stringWidth(s);
+            g.drawString(s, 950 - textWidth / 2, 200);
+            g.dispose();
+            bs.show();
+        }
+    }
+
+    public void tick() {
+        if (!gameover) {
+            ObjectHandler.tick();
+        }
 
     }
 
@@ -58,6 +89,30 @@ public class Game extends Canvas implements Runnable{
     public void run() {
         this.init();
         this.requestFocus();
-        this.test();
+        long pastTime = System.nanoTime();
+        double amountOfTicks = 1.0D;
+        double ns = 1.0E9D / amountOfTicks;
+        double delta = 0.0D;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(this.running) {
+            long now = System.nanoTime();
+            delta += (double)(now - pastTime) / ns;
+
+            for(pastTime = now; delta > 0.0D; --delta) {
+                this.tick();
+                ++updates;
+                this.render();
+                ++frames;
+            }
+
+            if (System.currentTimeMillis() - timer > 1000L) {
+                timer += 1000L;
+                System.out.println("FPS: " + frames + " | TICKS: " + updates);
+                updates = 0;
+                frames = 0;
+            }
+        }
     }
 }
--- src/main/java/at/altin/local/service/GraphicsLoader.java
+++ src/main/java/at/altin/local/service/GraphicsLoader.java
@@ -2,6 +2,8 @@ package at.altin.local.service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class GraphicsLoader {
     public GraphicsLoader() {
@@ -10,13 +12,20 @@ public class GraphicsLoader {
 
     public static BufferedImage loadGraphics(String path) {
         BufferedImage image = null;
-
         try {
             image = ImageIO.read(ResourceLoader.load("/" + path));
-        } catch (Exception var1) {
-            var1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
+        return image;
+    }
+    public static BufferedImage readGraphics(String path) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return image;
     }
 }
