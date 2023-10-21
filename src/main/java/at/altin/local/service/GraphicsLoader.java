7a03b62bbc23bb32d760cc2f9441e68b6cf45e14
altink7 <kelmendialtin48@gmail.com>
2023-10-21 13:20:24 +0200

    graphicLoader refactored

--- src/main/java/at/altin/local/service/GraphicsLoader.java
+++ src/main/java/at/altin/local/service/GraphicsLoader.java
@@ -5,7 +5,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GraphicsLoader {
